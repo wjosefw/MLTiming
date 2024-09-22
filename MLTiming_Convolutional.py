b594458c0bf4c61958b87c148d33991960d0d474
josea <javellan@ucm.es>
2024-09-22 22:50:41 +0200

    Eliminated best MAE metric

--- MLTiming_Convolutional.py
+++ MLTiming_Convolutional.py
@@ -1,21 +1,22 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
-# Data load
-V55= np.load('/home/josea/Co60_5cm_5cm.npy')
-V28= np.load('/home/josea/Co60_2cm_8cm.npy')
-V82= np.load('/home/josea/Co60_8cm_2cm.npy')
-REALS = np.concatenate((V28, V55, V82), axis = 0)
-
-#data = np.load('/home/josea/PRUEBA_Co60.npz')['data']
-data = np.load('/home/josea/pulsos_Na22_filt_norm_practica_polyfit.npz')['data']
-
 # Import functions
-from functions import (gauss, gauss_fit, create_and_delay_pulse_pair, create_position, calculate_gaussian_center_sigma, 
-                       plot_gaussian, get_gaussian_params, set_seed, interpolate_pulses)
+from functions import (create_and_delay_pulse_pair, create_position, 
+                       calculate_gaussian_center_sigma, plot_gaussian, get_gaussian_params, 
+                       set_seed, interpolate_pulses)
 from Models import ConvolutionalModel, train_loop_convolutional
 from functions_KAN import count_parameters
+
+
+# Load data 
+dir = '/home/josea/DEEP_TIMING/DEEP_TIMING_VS/Na22_filtered_data/'
+
+train_data = np.load(os.path.join(dir,'Na22_train.npz'))['data']
+val_data = np.load(os.path.join(dir, 'Na22_val.npz'))['data']
+test_data = np.load(os.path.join(dir, 'Na22_test_val.npz'))['data']
 
 
 # -------------------------------------------------------------------------
@@ -31,24 +32,31 @@ EXTRASAMPLING = 8
 start = 50*EXTRASAMPLING
 stop = 74*EXTRASAMPLING
 set_seed(42) #Fix seeds
-epochs = 750 
-lr = 1e-4
+epochs = 500
+lr = 1e-3
 
 
 # -------------------------------------------------------------------------
 #----------------------- INTERPOLATE PULSES -------------------------------
 # -------------------------------------------------------------------------
 
-new_data, new_time_step =  interpolate_pulses(data, EXTRASAMPLING = EXTRASAMPLING, time_step = 0.2)
-new_REALS, new_time_step =  interpolate_pulses(REALS, EXTRASAMPLING = EXTRASAMPLING, time_step = 0.2)
+
+new_train, new_time_step =  interpolate_pulses(train_data, EXTRASAMPLING = EXTRASAMPLING, time_step = 0.2)
+new_val, new_time_step =  interpolate_pulses(val_data, EXTRASAMPLING = EXTRASAMPLING, time_step = 0.2)
+new_test, new_time_step =  interpolate_pulses(test_data, EXTRASAMPLING = EXTRASAMPLING, time_step = 0.2)
 
 # Align the pulses 
 align_steps = 20
-new_data[:,:,1] = np.roll(new_data[:,:,1], align_steps)
-new_data[:,:align_steps,1] = np.random.normal(scale = 1e-6, size = align_steps)
 
+new_train[:,:,1] = np.roll(new_train[:,:,1], align_steps)
+new_val[:,:,1] = np.roll(new_val[:,:,1], align_steps)
+new_test[:,:,1] = np.roll(new_test[:,:,1], align_steps)
 
-print('New number of time points: %.d' % (new_data.shape[1]))
+new_train[:,:align_steps,1] = np.random.normal(scale = 1e-6, size = align_steps)
+new_val[:,:align_steps,1] = np.random.normal(scale = 1e-6, size = align_steps)
+new_test[:,:align_steps,1] = np.random.normal(scale = 1e-6, size = align_steps)
+
+print('New number of time points: %.d' % (new_train.shape[1]))
 print('New time step: %.4f' % (new_time_step))
 
 
@@ -56,8 +64,9 @@ print('New time step: %.4f' % (new_time_step))
 #----------------------- TRAIN/TEST SPLIT ---------------------------------
 # -------------------------------------------------------------------------
 
-train_data = new_data[:18000,:,:]
-test_data = new_data[18000:,:,:]
+train_data = new_train[:,start:stop,:] 
+validation_data = new_val[:,start:stop,:] 
+test_data = new_test[:,start:stop,:]
 print('Número de casos de entrenamiento: ', train_data.shape[0])
 print('Número de casos de test: ', test_data.shape[0])
 
@@ -79,8 +88,8 @@ print('Número de casos de test: ', test_data.shape[0])
 
 
 #---------------------------------------------- Delay real pulses --------------------------------
-train_dec0, REF_train_dec0 = create_and_delay_pulse_pair(train_data[:,start:stop,0], new_time_step, delay_steps = delay_steps, NOISE = True)
-train_dec1, REF_train_dec1 = create_and_delay_pulse_pair(train_data[:,start:stop,1], new_time_step, delay_steps = delay_steps, NOISE = True)
+train_dec0, REF_train_dec0 = create_and_delay_pulse_pair(train_data[:,:,0], new_time_step, delay_steps = delay_steps, NOISE = True)
+train_dec1, REF_train_dec1 = create_and_delay_pulse_pair(train_data[:,:,1], new_time_step, delay_steps = delay_steps, NOISE = True)
 
 # Create Dataset
 train_dataset_dec0 = torch.utils.data.TensorDataset(torch.from_numpy(train_dec0).float(), torch.from_numpy(np.expand_dims(REF_train_dec0, axis = -1)).float())
@@ -96,7 +105,7 @@ train_loader_dec1 = torch.utils.data.DataLoader(train_dataset_dec1, batch_size =
 
 
 #TEST_00 = np.stack((moved_pulses_test_dec0, moved_pulses_test_dec1), axis = 2)
-TEST_00 = test_data[:,start:stop,:]
+TEST_00 = test_data
 TEST_02 = create_position(TEST_00, channel_to_move = 1, channel_to_fix = 0, t_shift = t_shift, NOISE = True)
 TEST_20 = create_position(TEST_00, channel_to_move = 0, channel_to_fix = 1, t_shift = t_shift, NOISE = True)  
 TEST_04 = create_position(TEST_00, channel_to_move = 1, channel_to_fix = 0, t_shift = int(2*t_shift), NOISE = False)
@@ -164,12 +173,8 @@ if create_positions == True:
 
 # Get MAE
 Error = np.concatenate((error_V02, error_V20, error_V00, error_V04, error_V40), axis = 1)   
-Error_No_V00 = np.concatenate((error_V02, error_V20, error_V04, error_V40), axis = 1) 
 MAE = np.mean(Error, axis = 1)
-MAE_No_V00 = np.mean(Error_No_V00, axis = 1)
-
-idx_min_MAE = np.where(MAE_No_V00 == np.min(MAE_No_V00))[0][0]
-print(idx_min_MAE, MAE[idx_min_MAE])
+print(MAE[-1])
 
 # Plot
 plt.figure(figsize = (20,5))
@@ -181,8 +186,8 @@ plt.ylabel('Log10')
 plt.legend()
 
 plt.subplot(132)
-plt.hist(test_dec0[idx_min_MAE,:], bins = nbins, range = [-1, 5], alpha = 0.5, label = 'Detector 0');
-plt.hist(test_dec1[idx_min_MAE,:], bins = nbins, range = [-1, 5], alpha = 0.5, label = 'Detector 1');
+plt.hist(test_dec0[-1,:], bins = nbins, range = [-1, 5], alpha = 0.5, label = 'Detector 0');
+plt.hist(test_dec1[-1,:], bins = nbins, range = [-1, 5], alpha = 0.5, label = 'Detector 1');
 plt.title('Single detector prediction histograms')
 plt.xlabel('time (ns)')
 plt.ylabel('Counts')
@@ -198,30 +203,46 @@ plt.show()
 
 
 # Histogram and gaussian fit 
-# Histogram and gaussian fit 
-plot_gaussian(TOF_V04[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, label = '-0.4 ns offset', nbins = nbins)
-plot_gaussian(TOF_V02[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, label = '-0.2 ns offset', nbins = nbins)
-plot_gaussian(TOF_V00[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, label = ' 0.0 ns offset', nbins = nbins)
-plot_gaussian(TOF_V20[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, label = ' 0.2 ns offset', nbins = nbins)
-plot_gaussian(TOF_V40[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, label = ' 0.4 ns offset', nbins = nbins)
+plot_gaussian(TOF_V04[-1,:], centroid_V00[-1], range = 0.8, label = '-0.4 ns offset', nbins = nbins)
+plot_gaussian(TOF_V02[-1,:], centroid_V00[-1], range = 0.8, label = '-0.2 ns offset', nbins = nbins)
+plot_gaussian(TOF_V00[-1,:], centroid_V00[-1], range = 0.8, label = ' 0.0 ns offset', nbins = nbins)
+plot_gaussian(TOF_V20[-1,:], centroid_V00[-1], range = 0.8, label = ' 0.2 ns offset', nbins = nbins)
+plot_gaussian(TOF_V40[-1,:], centroid_V00[-1], range = 0.8, label = ' 0.4 ns offset', nbins = nbins)
 
 
-params_V04, errors_V04 = get_gaussian_params(TOF_V04[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, nbins = nbins)
-params_V02, errors_V02 = get_gaussian_params(TOF_V02[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, nbins = nbins)
-params_V00, errors_V00 = get_gaussian_params(TOF_V00[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, nbins = nbins)
-params_V20, errors_V20 = get_gaussian_params(TOF_V20[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, nbins = nbins)
-params_V40, errors_V40 = get_gaussian_params(TOF_V40[idx_min_MAE,:], centroid_V00[idx_min_MAE], range = 0.8, nbins = nbins)
+params_V04, errors_V04 = get_gaussian_params(TOF_V04[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
+params_V02, errors_V02 = get_gaussian_params(TOF_V02[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
+params_V00, errors_V00 = get_gaussian_params(TOF_V00[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
+params_V20, errors_V20 = get_gaussian_params(TOF_V20[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
+params_V40, errors_V40 = get_gaussian_params(TOF_V40[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
 
 
-print("V40: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V40[2], errors_V40[2], params_V40[3], errors_V40[3]))
-print("V20: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V20[2], errors_V20[2], params_V20[3], errors_V20[3]))
-print("V00: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V00[2], errors_V00[2], params_V00[3], errors_V00[3]))
-print("V02: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V02[2], errors_V02[2], params_V02[3], errors_V02[3]))
-print("V04: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V04[2], errors_V04[2], params_V04[3], errors_V04[3]))
+print("V40: CENTROID(ns) = %.4f +/- %.5f  FWHM(ns) = %.4f +/- %.5f" % (params_V40[2], errors_V40[2], params_V40[3], errors_V40[3]))
+print("V20: CENTROID(ns) = %.4f +/- %.5f  FWHM(ns) = %.4f +/- %.5f" % (params_V20[2], errors_V20[2], params_V20[3], errors_V20[3]))
+print("V00: CENTROID(ns) = %.4f +/- %.5f  FWHM(ns) = %.4f +/- %.5f" % (params_V00[2], errors_V00[2], params_V00[3], errors_V00[3]))
+print("V02: CENTROID(ns) = %.4f +/- %.5f  FWHM(ns) = %.4f +/- %.5f" % (params_V02[2], errors_V02[2], params_V02[3], errors_V02[3]))
+print("V04: CENTROID(ns) = %.4f +/- %.5f  FWHM(ns) = %.4f +/- %.5f" % (params_V04[2], errors_V04[2], params_V04[3], errors_V04[3]))
 
 print('')
 plt.legend()
-plt.xlabel('$\Delta t$ (ns)')
-plt.ylabel('Counts')
+plt.xlabel('$\Delta t$ (ns)', fontsize = 14)
+plt.ylabel('Counts', fontsize = 14)
 plt.show()
 
+import time
+
+time_test = np.tile(train_dec0[0,:,:], (1000000, 1,1))
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Start timer inference
+start_time_inference = time.time()
+with torch.no_grad():
+    assert not torch.is_grad_enabled()
+    output_time_test = model_dec0(torch.tensor(time_test[:,None, None, :,0]).float().to(device))
+end_time_inference = time.time()
+
+# Calculate the elapsed times
+elapsed_time_inference = end_time_inference - start_time_inference
+print(f"Elapsed time inference: {elapsed_time_inference} seconds")
