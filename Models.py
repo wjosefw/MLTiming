a23b94cfebac494dfbe42e69ef449a5f26964310
josea <javellan@ucm.es>
2024-10-24 18:06:45 +0200

    Added dropout and extra layer to convolutional model

--- Models.py
+++ Models.py
@@ -6,67 +6,27 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-from Losses import custom_loss_MAE, custom_loss_bounded
+from Losses import custom_loss_MAE, custom_loss_bounded, custom_loss_MSE, custom_loss_Threshold
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
-#
-#class ConvolutionalModel(nn.Module):
-#    def __init__(self, N_points):
-#        super(ConvolutionalModel, self).__init__()
-#        
-#        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 8, kernel_size = 3, padding = 1, stride = 1)
-#        self.bn1 = nn.BatchNorm2d(8)
-#        self.pool1 = nn.MaxPool2d((1, 2)) # Output: (batch_size, 8, 1, N_points // 2)
-#        
-#        self.conv2 = nn.Conv2d(in_channels = 8, out_channels = 16, kernel_size = 3, padding = 1, stride = 1)
-#        self.bn2 = nn.BatchNorm2d(16)
-#        self.pool2 = nn.MaxPool2d((1, 2))  # Output: (batch_size, 8, 1, N_points // 4)
-#        
-#        self.conv3 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 3, padding = 1, stride = 1)
-#        self.bn3 = nn.BatchNorm2d(32)
-#        self.pool3 = nn.MaxPool2d((1, 2))  # Output: (batch_size, 8, 1, N_points // 8)
-#        # Calculate the flattened size after the convolutions and pooling
-#        self.flatten_size = 32 * (N_points // 8)  # Adjust according to pooling layers
-#        
-#        self.fc1 = nn.Linear(self.flatten_size, 1)
-#    
-#    def forward(self, x):
-#        x = self.conv1(x)
-#        #x = self.bn1(x)
-#        #x = F.relu(x)
-#        x = self.pool1(x)
-#    
-#        x = self.conv2(x)
-#        #x = self.bn2(x)
-#        #x = F.relu(x)
-#        x = self.pool2(x)
-#
-#        x = self.conv3(x)
-#        #x = self.bn2(x)
-#        #x = F.relu(x)
-#        x = self.pool3(x)
-#        
-#        x = x.view(x.size(0), -1)  # Flatten
-#        x = self.fc1(x)
-#     
-#        return x
+
     
 class ConvolutionalModel(nn.Module):
     def __init__(self, N_points):
         super(ConvolutionalModel, self).__init__()
 
         # Convolutional Layer 1
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, padding=1, stride=1)
+        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 8, kernel_size = 3, padding = 1, stride = 1)
         self.bn1 = nn.BatchNorm2d(8)
         self.pool1 = nn.MaxPool2d((1, 2))  # Reduces width by half
         
         # Convolutional Layer 2
-        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1, stride=1)
+        self.conv2 = nn.Conv2d(in_channels = 8, out_channels = 16, kernel_size = 3, padding = 1, stride = 1)
         self.bn2 = nn.BatchNorm2d(16)
         self.pool2 = nn.MaxPool2d((1, 2))  # Reduces width by half
         
         # Convolutional Layer 3
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1, stride=1)
+        self.conv3 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 3, padding = 1, stride = 1)
         self.bn3 = nn.BatchNorm2d(32)
         self.pool3 = nn.MaxPool2d((1, 2))  # Reduces width by half
 
@@ -100,7 +60,83 @@ class ConvolutionalModel(nn.Module):
         x = self.fc1(x)
      
         return x
+
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------  
+
+def train_loop_convolutional(model, optimizer, train_loader, val_loader, test_tensor, EPOCHS = 75, name = 'model', save = False):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    test_tensor = test_tensor.to(device)
     
+    loss_list = []
+    val_loss_list = []
+    test = []
+
+    # Cosine Annealing Scheduler
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = EPOCHS)
+
+    for epoch in range(EPOCHS):
+        running_loss = 0.0
+        model.train()
+        for i, data in enumerate(train_loader):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            # Zero gradients
+            optimizer.zero_grad()
+
+            # Make predictions for this batch 
+            outputs_0 = model(inputs[:, None, None, :, 0])
+            outputs_1 = model(inputs[:, None, None, :, 1])
+
+            # Compute the loss and its gradients
+            loss = custom_loss_MAE(outputs_0, outputs_1, labels)
+            loss.backward()
+
+            # Adjust learning weights
+            optimizer.step()
+            
+            # Accumulate running loss
+            running_loss += loss.item()
+
+        # Step the scheduler
+        scheduler.step()
+
+        # Calculate average loss per epoch
+        avg_loss_epoch = running_loss / (i)  
+        loss_list.append(avg_loss_epoch)
+
+        print(f'EPOCH {epoch + 1}: LOSS train {avg_loss_epoch}')
+
+        # Calculate predictions on test_tensor
+        model.eval()
+        with torch.no_grad():
+            test_epoch = model(test_tensor[:,None, None, :])
+            test.append(np.squeeze(test_epoch.cpu().numpy()))
+
+            val_loss = 0
+            for val_data, val_labels in val_loader:
+                val_data, val_labels = val_data.to(device), val_labels.to(device)
+                val_0 = model(val_data[:, None, None, :, 0])
+                val_1 = model(val_data[:, None, None, :, 1])
+                val_loss += custom_loss_MAE(val_0, val_1, val_labels)
+        val_loss_list.append(val_loss.cpu().numpy() / len(val_loader))
+        print(f'LOSS val {val_loss / len(val_loader)}')
+
+
+    if save:
+        torch.save(model.state_dict(), name)
+
+    # Convert lists to numpy arrays
+    loss_array = np.array(loss_list, dtype = 'object')
+    test = np.array(test, dtype = 'object')
+    val = np.array(val_loss_list, dtype = 'object')
+    
+    return loss_array, test, val
+
+
+  
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 
@@ -227,80 +263,7 @@ def train_loop_MLP(model, optimizer,  train_loader, val_loader, test_tensor, EPO
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 
-def train_loop_convolutional(model, optimizer, train_loader, val_loader, test_tensor, EPOCHS = 75, name = 'model', save = False):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model.to(device)
-    test_tensor = test_tensor.to(device)
-    
-    loss_list = []
-    val_loss_list = []
-    test = []
 
-    # Cosine Annealing Scheduler
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = EPOCHS)
-
-    for epoch in range(EPOCHS):
-        running_loss = 0.0
-        model.train()
-        for i, data in enumerate(train_loader):
-            inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
-
-            # Zero gradients
-            optimizer.zero_grad()
-
-            # Make predictions for this batch 
-            outputs_0 = model(inputs[:, None, None, :, 0])
-            outputs_1 = model(inputs[:, None, None, :, 1])
-
-            # Compute the loss and its gradients
-            loss = custom_loss_MAE(outputs_0, outputs_1, labels)
-            loss.backward()
-
-            # Adjust learning weights
-            optimizer.step()
-            
-            # Accumulate running loss
-            running_loss += loss.item()
-
-        # Step the scheduler
-        scheduler.step()
-
-        # Calculate average loss per epoch
-        avg_loss_epoch = running_loss / (i)  
-        loss_list.append(avg_loss_epoch)
-
-        print(f'EPOCH {epoch + 1}: LOSS train {avg_loss_epoch}')
-
-        # Calculate predictions on test_tensor
-        model.eval()
-        with torch.no_grad():
-            test_epoch = model(test_tensor[:,None, None, :])
-            test.append(np.squeeze(test_epoch.cpu().numpy()))
-
-            val_loss = 0
-            for val_data, val_labels in val_loader:
-                val_data, val_labels = val_data.to(device), val_labels.to(device)
-                val_0 = model(val_data[:, None, None, :, 0])
-                val_1 = model(val_data[:, None, None, :, 1])
-                val_loss += custom_loss_MAE(val_0, val_1, val_labels)
-        val_loss_list.append(val_loss.cpu().numpy() / len(val_loader))
-        print(f'LOSS val {val_loss / len(val_loader)}')
-
-
-    if save:
-        torch.save(model.state_dict(), name)
-
-    # Convert lists to numpy arrays
-    loss_array = np.array(loss_list, dtype = 'object')
-    test = np.array(test, dtype = 'object')
-    val = np.array(val_loss_list, dtype = 'object')
-    
-    return loss_array, test, val
-
-
-#----------------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------------
 
 
 def train_loop_KAN(model, optimizer, train_loader, val_loader, test_tensor, EPOCHS = 75, checkpoint = 15, name = 'model', save = False):
