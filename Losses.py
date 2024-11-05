68fc5b995c61c4b4368e9f186576e052ff72929f
josea <javellan@ucm.es>
2024-11-05 12:25:52 +0100

    Removed penalty for negative times. Now i enforced in the model architecture instead.

--- Losses.py
+++ Losses.py
@@ -1,23 +1,18 @@
 import torch
 
 
-
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 
 def custom_loss_MAE(outputs_0, outputs_1, labels):
-    loss = (torch.mean(abs(outputs_0 - outputs_1 - labels)) +
-            torch.sum(torch.relu(-outputs_0)) +
-            torch.sum(torch.relu(-outputs_1)))
+    loss = torch.mean(abs(outputs_0 - outputs_1 - labels)) 
     return loss
 
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 
 def custom_loss_MSE(outputs_0, outputs_1, labels):
-    loss = (torch.mean((outputs_0 - outputs_1 - labels) ** 2) +  
-            torch.sum(torch.relu(-outputs_0)) +
-            torch.sum(torch.relu(-outputs_1)))
+    loss = torch.mean((outputs_0 - outputs_1 - labels) ** 2) 
     return loss
 
 
