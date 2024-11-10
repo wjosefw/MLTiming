2bcdc422c0c4b6f88a661ca09af986957eddad4b
josea <javellan@ucm.es>
2024-11-10 15:33:34 +0100

    Implementacion para usar threshold en cada detector por separado

--- MLTiming_Threshold_moments.py
+++ MLTiming_Threshold_moments.py
@@ -29,16 +29,16 @@ data_28 = np.load(os.path.join(dir, 'Na22_28_norm_ALBA.npz'))['data']
 #----------------------- IMPORTANT DEFINITIONS ----------------------------
 # -------------------------------------------------------------------------
 
-delay_time = 0.2                             # Max delay to training pulses in ns
+delay_time = 1                           # Max delay to training pulses in ns
 moments_order = 5                          # Order of moments used
 set_seed(42)                               # Fix seeds
-nbins = 71                                # Num bins for all histograms
+nbins = 71                                 # Num bins for all histograms
 t_shift = 1                                # Time steps to move for the new positions
 normalization_method = 'standardization'
 time_step = 0.2                            # Signal time step in ns
-epochs = 500                              # Number of epochs for training
+epochs = 1000                               # Number of epochs for training
 lr = 1e-3                                  # Model learning rate
-batch_size = 256                            # batch size used for training
+batch_size = 64                           # batch size used for training
 total_time = time_step*data_55.shape[1] - time_step  #time_step*train_data.shape[1] - time_step 
 save = False                               # Save models or not
 architecture = [moments_order, 5, 1, 1]   
@@ -47,52 +47,72 @@ window_low = 14                            # Number of steps to take before trig
 window_high = 10                           # Number of steps to take after trigger
 
 
-# -------------------------------------------------------------------------
-#----------------------- ALIGN PULSES -------------------------------------
-# -------------------------------------------------------------------------
+def extract_signal_along_time(vector, time_step, total_time, fraction = 0.2, window_low = 140, window_high = 10):
 
-#align_time = 0.56
-#new_train = continuous_delay(train_data, time_step = time_step, delay_time = align_time, channel_to_fix = 0, channel_to_move = 1)
-#new_val = continuous_delay(val_data, time_step = time_step, delay_time = align_time, channel_to_fix = 0, channel_to_move = 1)
-#new_test = continuous_delay(test_data, time_step = time_step, delay_time = align_time, channel_to_fix = 0, channel_to_move = 1)
+    new_vector = np.zeros((vector.shape[0], int(window_high + window_low)))
+    time_vector = np.zeros((vector.shape[0], int(window_high + window_low)))
+    t = np.arange(0, time_step*vector.shape[1], time_step) / total_time
+ 
+    for i in range(vector.shape[0]):
+        # Find indices where the signal in each channel exceeds the fraction threshold
+        index = np.where(vector[i,:] >= fraction)[0][0]
+             
+        # Calculate the low and high indices to extraction
+        index_low = index - window_low
+        index_high = index + window_high
 
-align_time = 0.1
-new_data_82 = continuous_delay(data_82, time_step = time_step, delay_time = align_time, channel_to_fix = 1, channel_to_move = 0)
-new_data_55 = continuous_delay(data_55, time_step = time_step, delay_time = align_time, channel_to_fix = 1, channel_to_move = 0)
-new_data_28 = continuous_delay(data_28, time_step = time_step, delay_time = align_time, channel_to_fix = 1, channel_to_move = 0)
+        # Extract cropped waveform and put into new vector
+        new_vector[i,:] =  vector[i,index_low:index_high]
+        time_vector[i,:] = t[index_low:index_high]
+        
 
-new_train = np.concatenate((new_data_55[:6000,:,:], new_data_28[:6000,:,:], new_data_82[:6000,:,:]), axis = 0)
-new_val = np.concatenate((new_data_55[6000:7000,:,:], new_data_28[6000:7000,:,:], new_data_82[6000:7000,:,:]), axis = 0)
-new_test = np.concatenate((new_data_55[6000:,:,:], new_data_28[6000:,:,:], new_data_82[6000:,:,:]), axis = 0)
+    return new_vector, time_vector
 
+new_train = np.concatenate((data_55[:6000,:,:], data_28[:6000,:,:], data_82[:6000,:,:]), axis = 0)
+new_val = np.concatenate((data_55[6000:7000,:,:], data_28[6000:7000,:,:], data_82[6000:7000,:,:]), axis = 0)
+new_test = np.concatenate((data_55[6000:,:,:], data_28[6000:,:,:], data_82[6000:,:,:]), axis = 0)
 
 #print('Número de casos de entrenamiento: ', train_data.shape[0])
 #print('Número de casos de test: ', test_data.shape[0])
+
 # -------------------------------------------------------------------------
 # -------------------- TRAIN/VALIDATION/TEST SET --------------------------
 # -------------------------------------------------------------------------
 
-train_array, train_time_array, a, b = extract_signal_along_time(new_train, time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
-val_array, val_time_array, _ , _    = extract_signal_along_time(new_val, time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
-test_array, test_time_array, _ , _  = extract_signal_along_time(new_test, time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
-print(a,b)
+#Extract pulses
+train_array_dec0, train_time_array_dec0 = extract_signal_along_time(new_train[:,:,0], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
+train_array_dec1, train_time_array_dec1 = extract_signal_along_time(new_train[:,:,1], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
+
+val_array_dec0, val_time_array_dec0 = extract_signal_along_time(new_val[:,:,0], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
+val_array_dec1, val_time_array_dec1 = extract_signal_along_time(new_val[:,:,1], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
+
+test_array_dec0, test_time_array_dec0  = extract_signal_along_time(new_test[:,:,0], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
+test_array_dec1, test_time_array_dec1  = extract_signal_along_time(new_test[:,:,1], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
+
+# Stack both detectors
+train_array = np.stack((train_array_dec0, train_array_dec1), axis = -1)
+train_time_array = np.stack((train_time_array_dec0, train_time_array_dec1), axis = -1)
+
+val_array = np.stack((val_array_dec0, val_array_dec1), axis = -1)
+val_time_array = np.stack((val_time_array_dec0, val_time_array_dec1), axis = -1)
+
+test_array = np.stack((test_array_dec0, test_array_dec1), axis = -1)
+test_time_array = np.stack((test_time_array_dec0, test_time_array_dec1), axis = -1)
+
+# Create virtual coincidences
 train_dec0, REF_train_dec0, time_train_dec0 = create_and_delay_pulse_pair_along_time(train_array[:,:,0], train_time_array[:,:,0], time_step, total_time, delay_time = delay_time)
 train_dec1, REF_train_dec1, time_train_dec1 = create_and_delay_pulse_pair_along_time(train_array[:,:,1], train_time_array[:,:,1], time_step, total_time, delay_time = delay_time)
 
 val_dec0, REF_val_dec0, val_time_dec0 = create_and_delay_pulse_pair_along_time(val_array[:,:,0], val_time_array[:,:,0], time_step, total_time, delay_time = delay_time)
 val_dec1, REF_val_dec1, val_time_dec1 = create_and_delay_pulse_pair_along_time(val_array[:,:,1], val_time_array[:,:,1], time_step, total_time, delay_time = delay_time)
 
-#TEST_00 = test_array 
-TEST_00 = test_array
-#TEST_02 = create_position(TEST_00, channel_to_move = 1, channel_to_fix = 0, t_shift = t_shift)
-#TEST_20 = create_position(TEST_00, channel_to_move = 0, channel_to_fix = 1, t_shift = t_shift)
-#TEST_04 = create_position(TEST_00, channel_to_move = 1, channel_to_fix = 0, t_shift = int(2*t_shift))
-#TEST_40 = create_position(TEST_00, channel_to_move = 0, channel_to_fix = 1, t_shift = int(2*t_shift))
+TEST = test_array 
 
 for i in range(test_array.shape[0]):
     plt.plot(test_array[i,:,0],'b-')
     plt.plot(test_array[i,:,1],'r-')
 plt.show()
+
 # Calculate moments
 M_Train_dec0 = momentos_threshold(train_dec0, time_train_dec0, order = moments_order) 
 M_Train_dec1 = momentos_threshold(train_dec1, time_train_dec1, order = moments_order) 
@@ -100,26 +120,7 @@ M_Train_dec1 = momentos_threshold(train_dec1, time_train_dec1, order = moments_o
 M_Val_dec0 = momentos_threshold(val_dec0, val_time_dec0, order = moments_order) 
 M_Val_dec1 = momentos_threshold(val_dec1, val_time_dec1, order = moments_order)
 
-MOMENTS_TEST_00 = momentos_threshold(TEST_00, test_time_array, order = moments_order)
-
-#MOMENTS_TEST_02_0 = momentos_threshold(TEST_02[:,:,0], test_time_array[:,:,0], order = moments_order)
-#MOMENTS_TEST_02_1 = momentos_threshold(TEST_02[:,:,1], test_time_array[:,:,1] + t_shift*time_step/total_time, order = moments_order)
-#MOMENTS_TEST_02 = np.stack((MOMENTS_TEST_02_0, MOMENTS_TEST_02_1), axis = -1)
-#
-#MOMENTS_TEST_04_0 = momentos_threshold(TEST_04[:,:,0], test_time_array[:,:,0], order = moments_order)
-#MOMENTS_TEST_04_1 = momentos_threshold(TEST_04[:,:,1], test_time_array[:,:,1] + 2*t_shift*time_step/total_time, order = moments_order)
-#MOMENTS_TEST_04 = np.stack((MOMENTS_TEST_04_0, MOMENTS_TEST_04_1), axis = -1)
-#                           
-#MOMENTS_TEST_20_1 = momentos_threshold(TEST_20[:,:,1], test_time_array[:,:,1], order = moments_order)
-#MOMENTS_TEST_20_0 = momentos_threshold(TEST_20[:,:,0], test_time_array[:,:,0] + t_shift*time_step/total_time, order = moments_order)
-#MOMENTS_TEST_20 = np.stack((MOMENTS_TEST_20_0, MOMENTS_TEST_20_1), axis = -1)
-#
-#MOMENTS_TEST_40_1 = momentos_threshold(TEST_40[:,:,1], test_time_array[:,:,1], order = moments_order)
-#MOMENTS_TEST_40_0 = momentos_threshold(TEST_40[:,:,0], test_time_array[:,:,0] + 2*t_shift*time_step/total_time, order = moments_order)
-#MOMENTS_TEST_40 = np.stack((MOMENTS_TEST_40_0, MOMENTS_TEST_40_1), axis = -1)
-
-#MOMENTS_TEST = np.concatenate((MOMENTS_TEST_02, MOMENTS_TEST_00, MOMENTS_TEST_20, MOMENTS_TEST_04, MOMENTS_TEST_40), axis = 0)
-MOMENTS_TEST = MOMENTS_TEST_00
+MOMENTS_TEST = momentos_threshold(TEST, test_time_array, order = moments_order)
 
 # Normalize moments 
 M_Train_dec0, params_dec0 =  normalize(M_Train_dec0, method = normalization_method)
@@ -138,8 +139,10 @@ MOMENTS_TEST_norm_dec1 = normalize_given_params(MOMENTS_TEST, params_dec1, chann
 MOMENTS_TEST = np.stack((MOMENTS_TEST_norm_dec0, MOMENTS_TEST_norm_dec1), axis = -1)
 
 plt.hist(M_Train_dec0[:,0,0], range = [-5, 5], bins = 71, alpha = 0.5);
+plt.hist(M_Train_dec1[:,0,0], range = [-5, 5], bins = 71, alpha = 0.5);
 plt.hist(MOMENTS_TEST[:,0,0], range = [-5, 5], bins = 71, alpha = 0.5);
 plt.show()
+
 
 # Create Dataset
 train_dataset_dec0 = torch.utils.data.TensorDataset(torch.from_numpy(M_Train_dec0).float(), torch.from_numpy(np.expand_dims(REF_train_dec0, axis = -1)).float())
@@ -182,29 +185,19 @@ loss_dec1, val_loss_dec1, test_dec1, val_dec1 = train_loop_KAN(model_dec1, optim
 # Calculate TOF
 TOF = test_dec0 - test_dec1
 
-#TOF_V02 = TOF[:,:TEST_00.shape[0]] 
-#TOF_V00 = TOF[:,TEST_00.shape[0] : 2*TEST_00.shape[0]] 
-#TOF_V20 = TOF[:,2*TEST_00.shape[0] :3*TEST_00.shape[0]] 
-#TOF_V04 = TOF[:,3*TEST_00.shape[0] :4*TEST_00.shape[0]] 
-#TOF_V40 = TOF[:,4*TEST_00.shape[0]:] 
-
-TOF_V00 = TOF[:,:new_data_55[6000:,:,:].shape[0]] 
-TOF_V02 = TOF[:, new_data_55[6000:,:,:].shape[0] : new_data_55[6000:,:,:].shape[0] + new_data_28[6000:,:,:].shape[0]] 
-TOF_V20 = TOF[:, new_data_55[6000:,:,:].shape[0]  + new_data_28[6000:,:,:].shape[0]:] 
+TOF_V00 = TOF[:,:data_55[6000:,:,:].shape[0]] 
+TOF_V02 = TOF[:, data_55[6000:,:,:].shape[0] : data_55[6000:,:,:].shape[0] + data_28[6000:,:,:].shape[0]] 
+TOF_V20 = TOF[:, data_55[6000:,:,:].shape[0]  + data_28[6000:,:,:].shape[0]:] 
         
-    
 # Calculate centered position 'centroid'
 centroid_V00 = calculate_gaussian_center(TOF_V00, nbins = nbins, limits = 3) 
 
 error_V02 = abs((TOF_V02 - centroid_V00[:, np.newaxis] + t_shift*time_step))
 error_V00 = abs((TOF_V00 - centroid_V00[:, np.newaxis]))
 error_V20 = abs((TOF_V20 - centroid_V00[:, np.newaxis] - t_shift*time_step))
-#error_V04 = abs((TOF_V04 - centroid_V00[:, np.newaxis] + 2*t_shift*time_step))
-#error_V40 = abs((TOF_V40 - centroid_V00[:, np.newaxis] - 2*t_shift*time_step))
 
 Error = np.concatenate((error_V02, error_V20, error_V00), axis = 1)
-#Error = np.concatenate((error_V02, error_V00, error_V20, error_V04, error_V40), axis = 1)   
-
+  
 # Print MAE
 MAE = np.mean(Error, axis = 1)
 print(MAE[-1])
@@ -238,23 +231,17 @@ plt.show()
 
 
 # Histogram and gaussian fit 
-#plot_gaussian(TOF_V04[-1,:], centroid_V00[-1], range = 0.8, label = '-0.4 ns offset', nbins = nbins)
 plot_gaussian(TOF_V02[-1,:], centroid_V00[-1], range = 0.8, label = '-0.2 ns offset', nbins = nbins)
 plot_gaussian(TOF_V00[-1,:], centroid_V00[-1], range = 0.8, label = ' 0.0 ns offset', nbins = nbins)
 plot_gaussian(TOF_V20[-1,:], centroid_V00[-1], range = 0.8, label = ' 0.2 ns offset', nbins = nbins)
-#plot_gaussian(TOF_V40[-1,:], centroid_V00[-1], range = 0.8, label = ' 0.4 ns offset', nbins = nbins)
 
-#params_V04, errors_V04 = get_gaussian_params(TOF_V04[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
 params_V02, errors_V02 = get_gaussian_params(TOF_V02[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
 params_V00, errors_V00 = get_gaussian_params(TOF_V00[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
 params_V20, errors_V20 = get_gaussian_params(TOF_V20[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
-#params_V40, errors_V40 = get_gaussian_params(TOF_V40[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
 
-#print("V40: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V40[2], errors_V40[2], params_V40[3], errors_V40[3]))
 print("V20: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V20[2], errors_V20[2], params_V20[3], errors_V20[3]))
 print("V00: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V00[2], errors_V00[2], params_V00[3], errors_V00[3]))
 print("V02: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V02[2], errors_V02[2], params_V02[3], errors_V02[3]))
-#print("V04: CENTROID(ns) = %.3f +/- %.3f  FWHM(ns) = %.3f +/- %.3f" % (params_V04[2], errors_V04[2], params_V04[3], errors_V04[3]))
 
 print('')
 plt.legend()
