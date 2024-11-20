eb5c214bdcf5e3707512202bd4e70e3abb17f803
josea <javellan@ucm.es>
2024-11-20 18:35:37 +0100

    Habilitated option to train to give LED time first and train with the pairs

--- MLTiming_Single_Det.py
+++ MLTiming_Single_Det.py
@@ -6,10 +6,34 @@ from efficient_kan.src.efficient_kan import KAN
 
 
 from functions import (extract_signal_along_time_singles, momentos_threshold, set_seed, 
-                       normalize, normalize_given_params, continuous_delay, 
+                       normalize, normalize_given_params, continuous_delay, calculate_slope_y_intercept,
                        plot_gaussian, get_gaussian_params, create_and_delay_pulse_pair_along_time)
 from Models import  count_parameters
-from Train_loops import train_loop_KAN
+from Train_loops import train_loop_KAN, train_loop_KAN_with_target
+
+def momentos(vector, order = 4):
+    
+    if  len(np.shape(vector)) == 2:
+        Nev, Nt = np.shape(vector)
+        t = np.linspace(0, 1, Nt)  # Normalized time array
+        MOMENT = np.zeros((Nev, order))
+        for i in range(Nev):
+                for j in range(order): 
+                    W = (t)**(j)
+                    MOMENT[i,j] = np.sum(W*vector[i,:])       
+   
+    
+    if  len(np.shape(vector)) == 3:
+        Nev, Nt, Nc = np.shape(vector) # Nev: Núm eventos, Nt: Núm puntos temporales, Nc: Número canales
+        t = np.linspace(0, 1, Nt)  # Normalized time array
+        MOMENT = np.zeros((Nev, order, Nc))
+        for i in range(Nev):
+            for j in range(order): # Number of moments used
+                for k in range(Nc):
+                    W = (t)**(j)
+                    MOMENT[i,j,k] = np.sum(W*vector[i,:,k])
+    
+    return MOMENT
 
 
 # Device setup
@@ -39,17 +63,14 @@ epochs = 50                               # Number of epochs for training
 lr = 1e-3                                  # Model learning rate
 batch_size = 16                            # batch size used for training
 total_time = time_step*data_55.shape[1] - time_step  
-save = True                                # Save models or not
+save = False                                # Save models or not
 save_name = 'KAN_models/model_dec' + str(channel)
 architecture = [moments_order, 5, 1, 1]   
-fraction = 0.01                            # Fraction to trigger the pulse cropping   
+fraction = 0.1                            # Fraction to trigger the pulse cropping   
 window_low = 10                            # Number of steps to take before trigger
 window_high = 10                           # Number of steps to take after trigger
 
 
-# -------------------------------------------------------------------------
-#----------------------- CROP WAVEFORM ------------------------------------
-# -------------------------------------------------------------------------
 
 new_train = np.concatenate((data_55[:6000,:,:], data_28[:6000,:,:], data_82[:6000,:,:]), axis = 0) 
 new_val = np.concatenate((data_55[6000:7000,:,:], data_28[6000:7000,:,:], data_82[6000:7000,:,:]), axis = 0)
@@ -59,13 +80,75 @@ new_test = np.concatenate((data_55[6000:,:,:], data_28[6000:,:,:], data_82[6000:
 #print('Número de casos de validacion: ', validation_data.shape[0])
 
 # -------------------------------------------------------------------------
-# -------------------- TRAIN/VALIDATION/TEST SET --------------------------
+# ---------------- TRAINING TO REPRODUCE LED ------------------------------
 # -------------------------------------------------------------------------
-
-#Extract pulses
 train_array, train_time_array = extract_signal_along_time_singles(new_train[:,:100,channel], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
 val_array, val_time_array = extract_signal_along_time_singles(new_val[:,:100,channel], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
 test_array, test_time_array = extract_signal_along_time_singles(new_test[:,:100,channel], time_step, total_time, fraction = fraction, window_low = window_low, window_high = window_high)
+
+#Get time stamps
+timestamps_train_list = []
+for i in range(train_array.shape[0]):
+  timestamp_train = calculate_slope_y_intercept(new_train[i,:,channel], time_step, threshold = fraction)
+  timestamps_train_list.append(timestamp_train)
+
+
+timestamps_val_list = []
+for i in range(new_val.shape[0]):
+  timestamp_val = calculate_slope_y_intercept(new_val[i,:,channel], time_step, threshold = fraction)
+  timestamps_val_list.append(timestamp_val)
+
+
+timestamps_test_list = []
+for i in range(new_test.shape[0]):
+  timestamp_test = calculate_slope_y_intercept(new_test[i,:,channel], time_step, threshold = fraction)
+  timestamps_test_list.append(timestamp_test)
+
+timestamps_train = np.array(timestamps_train_list)
+timestamps_val = np.array(timestamps_val_list)
+timestamps_test = np.array(timestamps_test_list)
+
+# Calculate moments
+M_Train = momentos_threshold(train_array, train_time_array, order = moments_order) 
+M_Val = momentos_threshold(val_array, val_time_array, order = moments_order) 
+MOMENTS_TEST = momentos_threshold(test_array, test_time_array, order = moments_order)
+
+
+# Normalize moments 
+M_Train, params =  normalize(M_Train, method = normalization_method)
+M_Val =  normalize_given_params(M_Val[:,:,None], params, channel = 0, method = normalization_method)
+MOMENTS_TEST = normalize_given_params(MOMENTS_TEST[:,:,None], params, channel = 0, method = normalization_method)
+
+print("Normalization parameters:", params)
+
+# Create Datasets/Dataloaders
+train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(M_Train).float(), torch.from_numpy(np.expand_dims(timestamps_train, axis = -1)).float())
+val_dataset = torch.utils.data.TensorDataset(torch.from_numpy(M_Val).float(), torch.from_numpy(np.expand_dims(timestamps_val, axis = -1)).float())
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size = len(val_dataset), shuffle = False)
+
+# -------------------------------------------------------------------------
+# ------------------------------ MODEL ------------------------------------
+# -------------------------------------------------------------------------
+
+
+model = KAN(architecture)
+print(f"Total number of parameters: {count_parameters(model)}")
+
+optimizer = torch.optim.AdamW(model.parameters(), lr = lr) 
+
+loss_target, val_loss_target, test_target, val_target = train_loop_KAN_with_target(model, optimizer, train_loader, val_loader, torch.tensor(MOMENTS_TEST).float(), EPOCHS = epochs, name = save_name, save = save) 
+
+plt.hist(test_target[-1,:] - timestamps_test, bins = nbins)
+plt.show()
+
+plt.hist(test_target[-1,:], bins = nbins)
+plt.show()
+
+# -------------------------------------------------------------------------
+# -------------------- SECOND TRAINING LOOP -------------------------------
+# -------------------------------------------------------------------------
 
 # Create virtual coincidences
 train, REF_train, time_train = create_and_delay_pulse_pair_along_time(train_array, train_time_array, time_step, total_time, delay_time = delay_time)
@@ -75,19 +158,12 @@ val, REF_val, val_time = create_and_delay_pulse_pair_along_time(val_array, val_t
 # Calculate moments
 M_Train = momentos_threshold(train, time_train, order = moments_order) 
 M_Val = momentos_threshold(val, val_time, order = moments_order) 
-MOMENTS_TEST = momentos_threshold(test_array, test_time_array, order = moments_order)
 
 # Normalize moments 
-#M_Train, params =  normalize(M_Train, method = normalization_method)
-#
-#M_Val_channel0 =  normalize_given_params(M_Val, params, channel = 0, method = normalization_method)
-#M_Val_channel1 =  normalize_given_params(M_Val, params, channel = 1, method = normalization_method)
-#M_Val = np.stack((M_Val_channel0, M_Val_channel1), axis = -1)
-#
-#MOMENTS_TEST_norm = normalize_given_params(MOMENTS_TEST[:,:,None], params, channel = 0, method = normalization_method)
-#
-#print("Normalization parameters:", params)
-
+M_Train, params =  normalize(M_Train, method = normalization_method)
+M_Val_channel0 =  normalize_given_params(M_Val, params, channel = 0, method = normalization_method)
+M_Val_channel1 =  normalize_given_params(M_Val, params, channel = 1, method = normalization_method)
+M_Val = np.stack((M_Val_channel0, M_Val_channel1), axis = -1)
 
 # Create Datasets/Dataloaders
 train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(M_Train).float(), torch.from_numpy(np.expand_dims(REF_train, axis = -1)).float())
@@ -98,15 +174,6 @@ val_loader = torch.utils.data.DataLoader(val_dataset, batch_size = len(val_datas
 
 
 
-# -------------------------------------------------------------------------
-# ------------------------------ MODEL ------------------------------------
-# -------------------------------------------------------------------------
-
-model = KAN(architecture)
-print(f"Total number of parameters: {count_parameters(model)}")
-
-optimizer = torch.optim.AdamW(model.parameters(), lr = lr) 
-  
 # Execute train loop
 loss, val_loss, test, val = train_loop_KAN(model, optimizer, train_loader, val_loader, torch.tensor(MOMENTS_TEST).float(), EPOCHS = epochs, name = save_name, save = save) 
 
@@ -122,7 +189,7 @@ plt.xlabel('Epochs')
 plt.legend()
 plt.show()
 
-plt.hist(test[-1,:], bins = nbins, alpha = 0.5, range = [0, 6], label = 'Detector 0');
+plt.hist(test[-1,:], bins = nbins, alpha = 0.5, label = 'Detector 0');
 plt.title('Single detector prediction histograms')
 plt.xlabel('time (ns)')
 plt.ylabel('Counts')
@@ -162,17 +229,17 @@ plt.ylabel('Validation Error')
 plt.xlabel('Validation target delay')
 plt.show()
 
-# Plot moment zero vs validation values
+# Plot moment one vs validation values
 plt.plot(MOMENTS_TEST_55, test_55, 'b.', label = '55')
 plt.plot(MOMENTS_TEST_28, test_28, 'r.', label = '28')
 plt.plot(MOMENTS_TEST_82, test_82, 'g.', label = '82')
 plt.xlabel('Moment one')
-plt.label('Time (ns)')
+plt.ylabel('Time (ns)')
 plt.legend()
 plt.show()
 
 # Plot representation of moments diff vs ref delay
-plt.plot(M_Val[:,1,0]- M_Val[:,1,1], REF_val,'b.')
+plt.plot(M_Val[:,1,0] - M_Val[:,1,1], REF_val, 'b.')
 plt.title('Diff moment vs ref delay')
 plt.show()
 
