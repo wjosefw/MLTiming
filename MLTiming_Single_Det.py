5b972ccf414ed1da332d9a6b0a3613e90dedca94
josea <javellan@ucm.es>
2025-01-08 16:16:55 +0100

    Added getting CFD Timestamps instead of LED

--- MLTiming_Single_Det.py
+++ MLTiming_Single_Det.py
@@ -6,8 +6,8 @@ from efficient_kan.src.efficient_kan import KAN
 
 
 from functions import (extract_signal_along_time_singles, momentos_threshold, set_seed, 
-                       normalize, normalize_given_params, calculate_slope_y_intercept,
-                       create_and_delay_pulse_pair_along_time, create_dataloaders)
+                       normalize, normalize_given_params, create_and_delay_pulse_pair_along_time, 
+                       create_dataloaders, Calculate_CFD)
 from Models import  count_parameters
 from Train_loops import train_loop_KAN, train_loop_KAN_with_target
 
@@ -41,14 +41,16 @@ set_seed(42)                               # Fix seeds
 nbins = 71                                 # Num bins for all histograms
 normalization_method = 'standardization'
 time_step = 0.2                            # Signal time step in ns
-epochs = 500                                # Number of epochs for training (first loop)
-epochs2 = 1                              # Number of epochs for training (second loop)
+epochs = 500                               # Number of epochs for training (first loop)
+epochs2 = 0                                # Number of epochs for training (second loop)
 lr = 1e-3                                  # Model learning rate
 batch_size = 32                            # batch size used for training 
 save = True                                # Save models or not
 save_name = 'KAN_models/model_dec' + str(channel)
 architecture = [moments_order, 3, 1, 1]   
-fraction = 0.1                             # Fraction to trigger the pulse cropping   
+fraction_pc = 0.1                          # Fraction to trigger the pulse cropping 
+fraction_cfd = 0.064                       # Fraction for CFD 
+shift = 6.8                                # Shift for CFD 
 window_low = 14                            # Number of steps to take before trigger
 window_high = 10                           # Number of steps to take after trigger
 
@@ -65,46 +67,27 @@ print('Número de casos de entrenamiento: ', train_data.shape[0])
 print('Número de casos de test: ', test_data.shape[0])
 
 # -------------------------------------------------------------------------
-# ---------------------- GET LED TIMESTAMPS -------------------------------
+# ---------------------- GET CFD TIMESTAMPS -------------------------------
 # -------------------------------------------------------------------------
 
-# Get time stamps
-timestamps_train_list = []
-for i in range(train_data.shape[0]):
-  timestamp_train = calculate_slope_y_intercept(train_data[i,:,channel], time_step, threshold = fraction)
-  timestamps_train_list.append(timestamp_train)
-
-
-timestamps_val_list = []
-for i in range(validation_data.shape[0]):
-  timestamp_val = calculate_slope_y_intercept(validation_data[i,:,channel], time_step, threshold = fraction)
-  timestamps_val_list.append(timestamp_val)
-
-
-timestamps_test_list = []
-for i in range(test_data.shape[0]):
-  timestamp_test = calculate_slope_y_intercept(test_data[i,:,channel], time_step, threshold = fraction)
-  timestamps_test_list.append(timestamp_test)
-
-timestamps_train = np.array(timestamps_train_list)
-timestamps_val = np.array(timestamps_val_list)
-timestamps_test = np.array(timestamps_test_list)
+# Get CFD timestamps
+timestamps_train = Calculate_CFD(train_data[:,:200, channel], fraction = fraction_cfd, shift = shift, time_step = time_step)
+timestamps_val = Calculate_CFD(validation_data[:,:200, channel], fraction = fraction_cfd, shift = shift, time_step = time_step)
+timestamps_test = Calculate_CFD(test_data[:,:200, channel], fraction = fraction_cfd, shift = shift, time_step = time_step)
 
 # -------------------------------------------------------------------------
 # ----------------- PREPARE DATA FOR FIRST LOOP ---------------------------
 # -------------------------------------------------------------------------
 
 # Extract pulses
-train_array, train_time_array = extract_signal_along_time_singles(train_data[:, :100, channel], time_step, fraction = fraction, window_low = window_low, window_high = window_high)
-val_array, val_time_array = extract_signal_along_time_singles(validation_data[:,:100,channel], time_step, fraction = fraction, window_low = window_low, window_high = window_high)
-test_array, test_time_array = extract_signal_along_time_singles(test_data[:,:100,channel], time_step, fraction = fraction, window_low = window_low, window_high = window_high)
-
+train_array, train_time_array = extract_signal_along_time_singles(train_data[:, :, channel], time_step, fraction = fraction_pc, window_low = window_low, window_high = window_high)
+val_array, val_time_array = extract_signal_along_time_singles(validation_data[:,:, channel], time_step, fraction = fraction_pc, window_low = window_low, window_high = window_high)
+test_array, test_time_array = extract_signal_along_time_singles(test_data[:,:, channel], time_step, fraction = fraction_pc, window_low = window_low, window_high = window_high)
 
 # Calculate moments
 M_Train = momentos_threshold(train_array, train_time_array, order = moments_order) 
 M_Val = momentos_threshold(val_array, val_time_array, order = moments_order) 
 M_Test = momentos_threshold(test_array, test_time_array, order = moments_order)
-
 
 # Normalize moments 
 M_Train, n_params =  normalize(M_Train, method = normalization_method)
@@ -139,6 +122,10 @@ plt.plot(np.log10(val_loss_target.astype('float32')), label = 'Val loss Detector
 plt.ylabel('Loss value (log)')
 plt.xlabel('Epochs')
 plt.legend()
+plt.show()
+
+val = np.squeeze(model(torch.tensor(M_Val).to(device).float()).cpu().detach().numpy())
+plt.plot(timestamps_val, val - timestamps_val, 'b.')
 plt.show()
 
 # -------------------------------------------------------------------------
@@ -195,7 +182,7 @@ plt.show()
 # -------------------------------------------------------------------------
 
 # Execute second train loop using the previously created virtual coincidences
-loss, val_loss, test, val = train_loop_KAN(model, optimizer, train_loader, val_loader, torch.tensor(M_Test).float(), EPOCHS = epochs2, name = save_name, save = save) 
+#loss, val_loss, test, val = train_loop_KAN(model, optimizer, train_loader, val_loader, torch.tensor(M_Test).float(), EPOCHS = epochs2, name = save_name, save = save) 
 
 
 # -------------------------------------------------------------------------
