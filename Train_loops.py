88e924272beb97c8b6b623ecb56e978488925223
josea <javellan@ucm.es>
2025-01-09 16:54:34 +0100

    added train_loop_convolutional_with_target

--- Train_loops.py
+++ Train_loops.py
@@ -103,6 +103,83 @@ def train_loop_convolutional(model, optimizer, train_loader, val_loader, test_te
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------  
 
+def train_loop_convolutional_with_target(model, optimizer, train_loader, val_loader, EPOCHS = 75, name = 'model', save = False):
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    
+    loss_list = []
+    val_loss_list = []
+  
+    # Define loss function
+    loss_fn = torch.nn.L1Loss()
+
+    # Cosine Annealing Scheduler
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = EPOCHS, eta_min = 1e-6)
+    
+    for epoch in range(EPOCHS):
+        running_loss = 0.0
+        model.train()
+
+        for data in train_loader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+           
+            # Zero gradients
+            optimizer.zero_grad()
+
+            # Make predictions for this batch 
+            outputs = model(inputs[:, None, :])
+            
+            # Compute the loss and its gradients
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+
+            # Adjust learning weights
+            optimizer.step()
+
+            # Accumulate running loss
+            running_loss += loss.item()
+
+        # Step the scheduler
+        scheduler.step()
+
+        # Calculate average loss per epoch
+        avg_loss_epoch = running_loss / len(train_loader)  
+        loss_list.append(avg_loss_epoch)
+
+        print(f'EPOCH {epoch + 1}: LOSS train {avg_loss_epoch}')
+
+        # Calculate predictions on test_tensor
+        model.eval()
+        with torch.no_grad():
+            val_loss = 0.0
+
+            for val_data, val_labels in val_loader:
+                val_data, val_labels = val_data.to(device), val_labels.to(device)
+                
+                mask = val_labels != 0 
+                val = model(val_data[:, None, :])
+                val_loss += loss_fn(val[mask], val_labels[mask]).item()
+    
+        
+            val_loss_list.append(val_loss / len(val_loader))
+            print(f'LOSS val {val_loss / len(val_loader)}')
+
+
+    if save:
+        torch.save(model.state_dict(), name)
+
+    # Convert lists to numpy arrays
+    loss_array = np.array(loss_list, dtype = 'object')
+    val_loss = np.array(val_loss_list, dtype = 'object')
+    
+    return loss_array, val_loss
+
+
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------  
+
 def train_loop_MLP(model, optimizer,  train_loader, val_loader, test_tensor, EPOCHS = 75, name = 'model', save = False):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -266,7 +343,7 @@ def train_loop_KAN_with_target(model, optimizer, train_loader, val_loader, EPOCH
     model.to(device)
     
      # Define loss
-    loss_fn = torch.nn.MSELoss()
+    loss_fn = torch.nn.L1Loss()
 
     # Define scheduler
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = EPOCHS, eta_min = 1e-6)
