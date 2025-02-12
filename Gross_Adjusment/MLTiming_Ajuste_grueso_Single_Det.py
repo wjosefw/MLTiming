8ee33d92a9e895ef36300dd4f484b16b6150674b
josea <javellan@ucm.es>
2025-02-12 15:13:00 +0100

    Updated comments

--- Gross_Adjusment/MLTiming_Ajuste_grueso_Single_Det.py
+++ Gross_Adjusment/MLTiming_Ajuste_grueso_Single_Det.py
@@ -1,9 +1,10 @@
+import sys
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-from efficient_kan.src.efficient_kan import KAN
 
+sys.path.append('/home/josea/DEEP_TIMING/DEEP_TIMING_VS/')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
@@ -11,14 +12,14 @@ print(device)
 from functions import (move_to_reference, create_and_delay_pulse_pair, 
                        set_seed, create_dataloaders, get_mean_pulse_from_set,
                        calculate_slope_y_intercept)
-from Models import ConvolutionalModel,  count_parameters
-from Train_loops import train_loop_convolutional_single_det
+from Models import ConvolutionalModel,  count_parameters, MLP_Torch
+from Train_loops import train_loop_convolutional_single_det, train_loop_MLP_Single_Det
 
 # Load data 
 dir = '/home/josea/Pulsos15CM20250130_version2/'
 
-data0_train = np.load(os.path.join(dir, 'Na22_norm_pos0_train.npz'))['data']
-data0_val = np.load(os.path.join(dir, 'Na22_norm_pos0_val.npz'))['data']
+data0_train = np.load(os.path.join(dir, 'Na22_norm_pos0_train.npz'), mmap_mode='r')['data']
+data0_val = np.load(os.path.join(dir, 'Na22_norm_pos0_val.npz'), mmap_mode='r')['data']
 
 
 # -------------------------------------------------------------------------
@@ -30,13 +31,14 @@ time_step = 0.2                 # Signal time step in ns
 set_seed(42)                    # Fix seeds
 nbins = 51                      # Num bins for all histograms
 lr = 1e-5
-epochs = 750
+epochs = 500
 batch_size = 32
 before  = 8                     # Points to take before threshold
 after = 5                       # Points to take after threshold
-channel = 1
+channel = 0
+Num_Neurons = 16
 save = True
-save_name = 'predictions/Convolutional/AG_model_dec' + str(channel)
+save_name = '../Trained_Models/AG_model_dec' + str(channel)
 
 # -------------------------------------------------------------------------
 #----------------------- TRAIN/TEST SPLIT ---------------------------------
@@ -52,7 +54,10 @@ print('Número de casos de validacion: ', validation_data.shape[0])
 # -------------------- TRAIN/VALIDATION/TEST SET --------------------------
 # -------------------------------------------------------------------------
 
+# Calculate mean pulse
 mean_pulse = get_mean_pulse_from_set(train_data, channel = channel)
+
+np.savez_compressed('../predictions/reference_pulse_dec' + str(channel) + '.npz', data = mean_pulse)
 
 # Get start and stop
 crossing = calculate_slope_y_intercept(mean_pulse, time_step, threshold = 0.1)
@@ -70,22 +75,23 @@ val, REF_val = create_and_delay_pulse_pair(moved_pulses_val, time_step, delay_ti
 
 # Create Dataloaders
 train_loader = create_dataloaders(train, REF_train, batch_size = batch_size, shuffle = True)
-val_loader  = create_dataloaders(val, REF_val, batch_size = batch_size, shuffle = False)
-
+val_loader  = create_dataloaders(val, REF_val, batch_size = val.shape[0], shuffle = False)
 
 # -------------------------------------------------------------------------
 # ------------------------------ MODEL ------------------------------------
 # -------------------------------------------------------------------------
 
 set_seed(42)
-model = ConvolutionalModel(int(stop - start))
+#model = ConvolutionalModel(int(stop - start))
+model = MLP_Torch(NM = int(stop- start), NN = Num_Neurons, STD_INIT = 0.5)
 
 print(f"Total number of parameters: {count_parameters(model)}")
 
 optimizer = torch.optim.AdamW(model.parameters(), lr = lr, weight_decay = 1e-5) 
 
 # Execute train loop
-loss, val_loss, val = train_loop_convolutional_single_det(model, optimizer, train_loader, val_loader, EPOCHS = epochs, name = save_name,  save = save) 
+#loss, val_loss, val = train_loop_convolutional_single_det(model, optimizer, train_loader, val_loader, EPOCHS = epochs, name = save_name,  save = save) 
+loss, val_loss, val = train_loop_MLP_Single_Det(model, optimizer, train_loader, val_loader, EPOCHS = epochs, name = '../Trained_Models/MLPWAVE_AG_model_dec' + str(channel), save = save)
 
 # -------------------------------------------------------------------------
 # ------------------------------ RESULTS ----------------------------------
