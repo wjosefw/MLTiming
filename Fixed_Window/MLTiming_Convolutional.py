f96495c53ef49a8cd9377ce0f5e7c2fcdf34ee13
josea <javellan@ucm.es>
2025-03-24 18:30:10 +0100

    simplified workfow using dataset class

--- Fixed_Window/MLTiming_Convolutional.py
+++ Fixed_Window/MLTiming_Convolutional.py
@@ -7,8 +7,8 @@ import sys
 # Import Hyperparameters and Paths
 from config_Fixed_Window import (device, delay_time, time_step, nbins, 
                                  seed, epochs, lr, batch_size, save, 
-                                 positions, start, stop, BASE_DIR,
-                                 MODEL_SAVE_DIR)
+                                 positions, start, stop, Theoretical_TOF,
+                                 BASE_DIR, MODEL_SAVE_DIR, DATA_DIR, step_size)
 
 print(device)
 sys.path.append(str(BASE_DIR.parent))
@@ -18,21 +18,18 @@ from functions import (create_and_delay_pulse_pair, calculate_gaussian_center,
                        plot_gaussian, get_gaussian_params, set_seed,
                        create_dataloaders)
 from Models import ConvolutionalModel,  count_parameters
+from Dataset import Datos_LAB_GFN
 from Train_loops import train_loop_convolutional
 
-
-# Load data 
-dir = '/home/josea/PracticaTimingDigital'
-data = np.load(os.path.join(dir, 'pulsoNa22:filt_norm.npz'))['data']
-set_seed(seed)                 # Fix seeds
-
 # -------------------------------------------------------------------------
-#----------------------- TRAIN/TEST SPLIT ---------------------------------
+#---------------------------- LOAD DATA -----------------------------------
 # -------------------------------------------------------------------------
 
-train_data = data[:10000,:,:]
-validation_data = data[:1,:,:]
-test_data = data[10000:,:,:]
+dataset = Datos_LAB_GFN(data_dir = DATA_DIR, positions = positions, step_size = step_size)
+
+train_data = np.load(os.path.join(DATA_DIR, 'Na22_norm_pos0_train.npz'), mmap_mode = 'r')['data']
+validation_data = np.load(os.path.join(DATA_DIR, 'Na22_norm_pos0_val.npz'), mmap_mode = 'r')['data']
+test_data = dataset.load_data()
 
 train_data = train_data[:,start:stop,:]
 validation_data = validation_data[:,start:stop,:] 
@@ -40,6 +37,7 @@ test_data = test_data[:,start:stop,:]
 
 print('Número de casos de entrenamiento: ', train_data.shape[0])
 print('Número de casos de test: ', test_data.shape[0])
+set_seed(seed)                    # Fix seeds
 
 # -------------------------------------------------------------------------
 # -------------------- TRAIN/VALIDATION/TEST SET --------------------------
@@ -55,8 +53,8 @@ val_dec1, REF_val_dec1 = create_and_delay_pulse_pair(validation_data[:,:,1], tim
 train_loader_dec0 = create_dataloaders(train_dec0, REF_train_dec0, batch_size = batch_size, shuffle = True)
 train_loader_dec1 = create_dataloaders(train_dec1, REF_train_dec1, batch_size = batch_size, shuffle = True)
 
-val_loader_dec0  = create_dataloaders(val_dec0, REF_val_dec0, batch_size = batch_size, shuffle = False)
-val_loader_dec1  = create_dataloaders(val_dec1, REF_val_dec1, batch_size = batch_size, shuffle = False)
+val_loader_dec0  = create_dataloaders(val_dec0, REF_val_dec0, batch_size =  val_dec0.shape[0], shuffle = False)
+val_loader_dec1  = create_dataloaders(val_dec1, REF_val_dec1, batch_size =  val_dec1.shape[0], shuffle = False)
 
 # -------------------------------------------------------------------------
 # ------------------------------ MODEL ------------------------------------
@@ -69,35 +67,31 @@ model_dec1 = ConvolutionalModel(int(stop-start))
 
 print(f"Total number of parameters: {count_parameters(model_dec0)}")
 
-optimizer_dec0 = torch.optim.AdamW(model_dec0.parameters(), lr = lr, weight_decay = 1e-5) 
-optimizer_dec1 = torch.optim.AdamW(model_dec1.parameters(), lr = lr, weight_decay = 1e-5) 
+optimizer_dec0 = torch.optim.AdamW(model_dec0.parameters(), lr = lr) 
+optimizer_dec1 = torch.optim.AdamW(model_dec1.parameters(), lr = lr) 
 
 #Execute train loop
-loss_dec0, val_loss_dec0, test_dec0, val_dec0 = train_loop_convolutional(model_dec0, optimizer_dec0, train_loader_dec0, val_loader_dec0, torch.tensor(test_data[:,:,0]).float(), EPOCHS = epochs, name = 'Trained_Models/Fixed_Window_model_dec0',  save = save) 
-loss_dec1, val_loss_dec1, test_dec1, val_dec1 = train_loop_convolutional(model_dec1, optimizer_dec1, train_loader_dec1, val_loader_dec1, torch.tensor(test_data[:,:,1]).float(), EPOCHS = epochs, name = 'Trained_Models/Fixed_Window_model_dec1',  save = save)
+loss_dec0, val_loss_dec0, test_dec0, val_dec0 = train_loop_convolutional(model_dec0, optimizer_dec0, train_loader_dec0, val_loader_dec0, torch.tensor(test_data[:,:,0]).float(), EPOCHS = epochs, name = os.path.join(MODEL_SAVE_DIR, 'Fixed_Window_model_dec0'),  save = save) 
+loss_dec1, val_loss_dec1, test_dec1, val_dec1 = train_loop_convolutional(model_dec1, optimizer_dec1, train_loader_dec1, val_loader_dec1, torch.tensor(test_data[:,:,1]).float(), EPOCHS = epochs, name = os.path.join(MODEL_SAVE_DIR, 'Fixed_Window_model_dec1'),  save = save)
 
 # -------------------------------------------------------------------------
 # ------------------------------ RESULTS ----------------------------------
 # -------------------------------------------------------------------------
 
-# Calculate TOF
-TOF = test_dec0 - test_dec1
-print(TOF.shape)
-TOF_V00 = TOF
+# Calculate TOF and decompress
+TOF = test_dec0 - test_dec1 
 
-   
-# Calulate Validation error
-centroid_V00 = calculate_gaussian_center(TOF_V00, nbins = nbins, limit = 3) 
-    
-error_V00 = abs((TOF_V00 - centroid_V00[:, np.newaxis] - positions[1]))
+size = int(TOF.shape[1]/Theoretical_TOF.shape[0]) # Size of slice
+TOF_dict = dataset.get_TOF_slices_train(TOF, size)
 
-# Get MAE
-#Error = np.concatenate((error_V02, error_V20, error_V00),  axis = 1)  
-#MAE = np.mean(Error, axis = 1)
-MAE = np.mean(error_V00, axis = 1)
+# Calulate Error
+centroid_V00 = calculate_gaussian_center(TOF_dict[0], nbins = nbins, limit = 6) 
+
+error_dict = dataset.compute_error(centroid_V00[:,np.newaxis]) # Get error of each position
+Error = np.concatenate(list(error_dict.values()), axis = 1)   # Concatenate all positions
+MAE = np.mean(Error, axis = 1)
 print(MAE[-1])
-
-
+   
 # Plot
 plt.figure(figsize = (20,5))
 plt.subplot(131)
@@ -126,71 +120,14 @@ plt.legend()
 plt.show()
 
 # Histogram and gaussian fit 
-
-plot_gaussian(TOF_V00[-1,:], centroid_V00[-1], range = 0.8, label = ' 0.0 ns offset', nbins = nbins)
-
-
-params_V00, errors_V00 = get_gaussian_params(TOF_V00[-1,:], centroid_V00[-1], range = 0.8, nbins = nbins)
-
-print("V00: CENTROID(ns) = %.4f +/- %.5f  FWHM(ns) = %.4f +/- %.5f" % (params_V00[1], errors_V00[2], params_V00[2], errors_V00[3]))
+plt.figure(figsize = (16, 6))
+for i in range(np.min(positions), np.max(positions) + 1):     
+    plot_gaussian(TOF_dict[i][-1,:], centroid_V00[-1], range = 0.6, label = 'pos' + str(i), nbins = nbins)
+    params, errors = get_gaussian_params(TOF_dict[i][-1,:], centroid_V00[-1], range = 0.6, nbins = nbins)
+    print(f"{i}: CENTROID(ns) = {params[1]:.4f} +/- {errors[2]:.5f}  FWHM(ns) = {params[2]:.4f} +/- {errors[3]:.5f}")
 
 print('')
 plt.legend()
-plt.xlabel('$\Delta t$ (ns)', fontsize = 14)
+plt.xlabel(r'\Delta t$ (ns)', fontsize = 14)
 plt.ylabel('Counts', fontsize = 14)
 plt.show()
-
-#centroid_V00 = calculate_gaussian_center(TOF_V00, nbins = nbins, limits = 3) 
-#centroid_V02 = calculate_gaussian_center(TOF_V02 - centroid_V00[:, np.newaxis], nbins = nbins, limits = 3) 
-#centroid_V20 = calculate_gaussian_center(TOF_V20 - centroid_V00[:, np.newaxis], nbins = nbins, limits = 3)
-#
-#error_V20_centroid = abs(centroid_V20 - 0.2)
-#error_V02_centroid = abs(centroid_V02 + 0.2)
-#
-#avg_bias = np.mean(np.stack((error_V20_centroid , error_V02_centroid), axis = -1), axis = 1)
-#
-#
-#Plot MAE_singles vs MAE_coincidences
-#err_val_dec0 = abs(val_dec0[:,:,0] - val_dec0[:,:,1] - REF_val_dec0[np.newaxis,:])
-#err_val_dec1 = abs(val_dec1[:,:,0] - val_dec1[:,:,1] - REF_val_dec1[np.newaxis,:])
-#mean_err_val_dec0 = np.mean(err_val_dec0, axis = 1)
-#mean_err_val_dec1 = np.mean(err_val_dec1, axis = 1)
-#np.savez_compressed('/home/josea/DEEP_TIMING/DEEP_TIMING_VS/predictions/mean_err_val_dec0_Na22.npz', data = mean_err_val_dec0)
-#np.savez_compressed('/home/josea/DEEP_TIMING/DEEP_TIMING_VS/predictions/mean_err_val_dec1_Na22.npz', data = mean_err_val_dec1)
-#np.savez_compressed('/home/josea/DEEP_TIMING/DEEP_TIMING_VS/predictions/MAE_Na22.npz', data = MAE)
-#np.savez_compressed('/home/josea/DEEP_TIMING/DEEP_TIMING_VS/predictions/avg_bias.npz', data = avg_bias)
-
-
-#CTR = []
-#for i in range(epochs):
-#    params_V02, errors_V02 = get_gaussian_params(TOF_V02[i,:], centroid_V00[i], range = 0.8, nbins = nbins)
-#    params_V00, errors_V00 = get_gaussian_params(TOF_V00[i,:], centroid_V00[i], range = 0.8, nbins = nbins)
-#    params_V20, errors_V20 = get_gaussian_params(TOF_V20[i,:], centroid_V00[i], range = 0.8, nbins = nbins)
-#    CTR.append(np.mean([params_V20[3],params_V00[3],params_V02[3]]))
-#np.savez_compressed('/home/josea/DEEP_TIMING/DEEP_TIMING_VS/predictions/ctr.npz', data = np.array(CTR))
-#
-
-#### Combine the two numbers
-#num = f"{sys.argv[1]}{sys.argv[2]}"
-#
-## Your existing variables
-#FWHM = np.array([params_V02[3], params_V00[3], params_V20[3]])  # ps
-#FWHM_err = np.array([errors_V02[3],  errors_V00[3],  errors_V20[3]])        # ps
-#centroid = np.array([params_V02[2], params_V00[2], params_V20[2]])  # ps
-#centroid_err = np.array([errors_V02[2],  errors_V00[2],  errors_V20[2]])        # ps
-#
-## Multiply by 1000
-#FWHM = FWHM * 1000
-#FWHM_err = FWHM_err * 1000
-#centroid = centroid * 1000
-#centroid_err = centroid_err * 1000
-#
-#
-## Open the file in append mode
-#with open('results_FS.txt', 'a') as file:
-#    file.write(f"FWHM_{num} = np.array([{', '.join(f'{v:.1f}' for v in FWHM)}])  # ps\n")
-#    file.write(f"FWHM_err_{num} = np.array([{', '.join(f'{v:.1f}' for v in FWHM_err)}])  # ps\n")
-#    file.write(f"centroid_{num} = np.array([{', '.join(f'{v:.1f}' for v in centroid)}])  # ps\n")
-#    file.write(f"centroid_err_{num} = np.array([{', '.join(f'{v:.1f}' for v in centroid_err)}])  # ps\n")
-#    file.write(f"MAE_{num} = {MAE[-1]:.7f}  # ps\n")  # Write MAE with one decima
-#    file.write("\n")  # Add a new line for better separation
