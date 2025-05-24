17080c94e2174a2acb99cbcca049be5727314670
josea <javellan@ucm.es>
2025-05-24 16:37:45 +0200

    sen time_step and positions to dataset class

--- Gross_Adjusment/config_Gross_Adjustment.py
+++ Gross_Adjusment/config_Gross_Adjustment.py
@@ -8,7 +8,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Training parameters
 batch_size = 32
-epochs = 500
+epochs = 50
 learning_rate = 1e-5
 Num_Neurons = 16
 normalization_method = 'min-max'
@@ -16,12 +16,8 @@ moments_order = 3
 architecture = [moments_order, 5, 1, 1]    # KAN architecture
 
 # Data settings
-time_step = 0.2  # Signal time step in ns
 delay_time = 1   # Max delay to training pulses in ns
 nbins = 51       # Number of histogram bins
-positions = np.array([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]) 
-step_size = 0.066 # 1 cm is a TOF difference of 66.6 ps 
-Theoretical_TOF = step_size*positions
 before = 8
 after = 5
 threshold = 0.1  # Reference threshold crop pulses
